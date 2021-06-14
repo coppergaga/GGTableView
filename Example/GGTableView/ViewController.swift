e20a135344c671820c62c421e1ae2bca6288596f
coppergaga <32791283+coppergaga@users.noreply.github.com>
2021-06-14 17:54:49 +0800

    imporve functions
    *forward more delegate methods
     -tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath)
     -tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath)
     -tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath)
    
    *support section header footer view
    *forward some scrollview delegate
    *provide some accessors to datasource

--- Example/GGTableView/ViewController.swift
+++ Example/GGTableView/ViewController.swift
@@ -16,13 +16,14 @@ class ViewController: UIViewController {
         var tableView = UITableView()
         tableView.delegate = proxy
         tableView.dataSource = proxy
-        tableView.rowHeight = UITableView.automaticDimension
         tableView.estimatedRowHeight = 44.0
         tableView.estimatedSectionHeaderHeight = 0.0
         tableView.estimatedSectionFooterHeight = 0.0
         return tableView
     }()
-    var proxy = GGTableViewDelegateProxy()
+    
+    let proxy: GGTableViewDelegateProxy = GGTableViewDelegateProxy()
+    var section = GGTableViewSection()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -31,16 +32,30 @@ class ViewController: UIViewController {
             make.edges.equalTo(view)
         }
         
-        let section = GGTableViewSection()
-        for _ in 0..<10 {
-            let row = GGTestRow()
-            row.didSelectRow = {[unowned self] idxPath in
-                self.navigationController?.pushViewController(GGTestViewController(), animated: true)
+        proxy.tableView = tableView
+        proxy.enableForwardScrollMsg = true
+        
+        let headerView = GGTestHeaderView(frame: CGRect(x: 0, y: 0, width: 100, height: 10))
+        headerView.eventClouse = {
+            self.section.headerHeight = CGFloat.zero
+            self.section.tableView?.performBatchUpdates(nil, completion: nil)
+        }
+        section.sectionHeaderView = headerView
+        
+        for idx in 0..<10 {
+            let row: GGTableViewRow = GGTestRow()
+            if idx != 0 {
+                row.scrollViewDidScroll = { (scrollView, decelerate) in
+                    print("receive scrollview did scroll msg")
+                }
             }
-            section.rowsArray.append(row)
+            row.didSelectRow = {[unowned self] (tv, cell, idxPath) in
+                self.navigationController?.pushViewController(ViewController(), animated: true)
+            }
+            section.add(row: row)
         }
         
-        proxy.dataSource.append(section)
+        proxy.dataSource.add(section: section)
         
         tableView.reloadData()
         
@@ -51,5 +66,62 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+
 }
 
+class GGTestHeaderView: UIView {
+    var eventClouse: (() -> Void)?
+    
+    let btn1 = UIButton()
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        self.backgroundColor = UIColor.gray
+        
+        btn1.backgroundColor = UIColor.red
+        btn1.setTitle("higher", for: .normal)
+        btn1.addTarget(self, action: #selector(higherEvent), for: .touchUpInside)
+        btn1.setTitleColor(UIColor.black, for: .normal)
+        let btn2 = UIButton()
+        btn2.backgroundColor = UIColor.yellow
+        btn2.setTitle("shorter", for: .normal)
+        btn2.addTarget(self, action: #selector(shorterEvent), for: .touchUpInside)
+        btn2.setTitleColor(UIColor.black, for: .normal)
+        
+        self.addSubview(btn1)
+        self.addSubview(btn2)
+        btn1.snp.makeConstraints { make in
+            make.top.equalTo(self).offset(20)
+            make.bottom.equalTo(self).offset(-30).priority(.high)
+            make.left.equalTo(self).offset(10)
+            make.width.equalTo(100)
+            make.height.equalTo(40)
+        }
+        btn2.snp.makeConstraints { make in
+            make.centerY.equalTo(self)
+            make.left.greaterThanOrEqualTo(btn1).offset(10)
+            make.right.equalTo(self).offset(-10).priority(.high)
+            make.width.equalTo(100)
+
+        }
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    @objc
+    func higherEvent() {
+        btn1.snp.updateConstraints { make in
+            make.top.equalTo(40)
+        }
+        eventClouse?()
+    }
+    @objc
+    func shorterEvent() {
+        btn1.snp.updateConstraints { make in
+            make.top.equalTo(0)
+        }
+        eventClouse?()
+    }
+}
--- GGTableView/Classes/GGTableView.swift
+++ GGTableView/Classes/GGTableView.swift
@@ -7,14 +7,26 @@
 
 import Foundation
 import UIKit
+import Swift
+
+public typealias GGTableViewProxyBlock = (UITableView, UITableViewCell?, IndexPath) -> Void
+public typealias GGTableViewScrollBlock = (UIScrollView, Bool?) -> Void
 
 //MARK:- datasource
 open class GGTableViewDelegateProxy: NSObject {
-    open var dataSource: Array<GGTableViewSection> = []
+    open var dataSource = GGTableViewDataSource()
     
-    final private func findRow(at indexPath: IndexPath) -> GGTableViewRow {
-        let section = dataSource[safe: indexPath.section]
-        let row = section.rowsArray[safe: indexPath.row]
+    public var enableForwardScrollMsg = false
+    
+    public weak var tableView: UITableView? {
+        didSet {
+            dataSource.tableView = self.tableView
+        }
+    }
+    
+    final public func findRow(at indexPath: IndexPath) -> GGTableViewRow {
+        let section = dataSource[indexPath.section]
+        let row = section[indexPath.row]
         return row
     }
 }
@@ -28,8 +40,61 @@ extension GGTableViewDelegateProxy: UITableViewDelegate {
     public func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         let row = findRow(at: indexPath)
         if let selectBlock = row.didSelectRow {
-            selectBlock(indexPath)
+            selectBlock(tableView, nil, indexPath)
         }
+    }
+    
+    public func tableView(_ tableView: UITableView, didDeselectRowAt indexPath: IndexPath) {
+        let row = findRow(at: indexPath)
+        if let deselectBlock = row.didDeselectRow {
+            deselectBlock(tableView, nil, indexPath)
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, willDisplay cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let row = findRow(at: indexPath)
+        if !row.isVisiable {
+            row.isVisiable = true
+        }
+        if let willDisplayBlock = row.willDisplayCell {
+            willDisplayBlock(tableView, cell, indexPath)
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, didEndDisplaying cell: UITableViewCell, forRowAt indexPath: IndexPath) {
+        let row = findRow(at: indexPath)
+        if row.isVisiable {
+            row.isVisiable = false
+        }
+        if let endDisplayingBlock = row.didEndDisplayingCell {
+            endDisplayingBlock(tableView, cell, indexPath)
+        }
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForHeaderInSection section: Int) -> UIView? {
+        let sec = dataSource[section]
+        return sec.sectionHeaderView
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForHeaderInSection section: Int) -> CGFloat {
+        let sec = dataSource[section]
+        if let headerView = sec.sectionHeaderView, sec.headerHeight <= CGFloat.zero {
+            sec.headerHeight = headerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+        }
+        return sec.headerHeight
+    }
+    
+    public func tableView(_ tableView: UITableView, viewForFooterInSection section: Int) -> UIView? {
+        let sec = dataSource[section]
+        return sec.sectionFooterView
+    }
+    
+    public func tableView(_ tableView: UITableView, heightForFooterInSection section: Int) -> CGFloat {
+        let sec = dataSource[section]
+        if let footerView = sec.sectionFooterView, sec.footerHeight <= CGFloat.zero {
+            sec.footerHeight = footerView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+        }
+        return sec.footerHeight
     }
 }
 
@@ -39,15 +104,69 @@ extension GGTableViewDelegateProxy: UITableViewDataSource {
     }
     
     public func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        dataSource[safe: section].rowsArray.count
+        dataSource[section].count
     }
     
     public func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         let row = findRow(at: indexPath)
         let cell = row.getCell(for: tableView, at: indexPath)
         row.update(cell, at: indexPath)
-        row.height = cell.contentView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+        if row.height <= 0 {
+            row.height = cell.contentView.systemLayoutSizeFitting(UIView.layoutFittingCompressedSize).height
+        }
         return cell
+    }
+}
+
+extension GGTableViewDelegateProxy {
+    public func scrollViewDidScroll(_ scrollView: UIScrollView) {
+        guard enableForwardScrollMsg else {
+            return
+        }
+        if let tv = scrollView as? UITableView, let proxy = tv.delegate as? GGTableViewDelegateProxy {
+            let _ = proxy.dataSource.allRows
+                        .compactMap { $0.isVisiable ? $0.scrollViewDidScroll?(scrollView, nil) : nil }
+        }
+    }
+    
+    public func scrollViewWillBeginDragging(_ scrollView: UIScrollView) {
+        guard enableForwardScrollMsg else {
+            return
+        }
+        if let tv = scrollView as? UITableView, let proxy = tv.delegate as? GGTableViewDelegateProxy {
+            let _ = proxy.dataSource.allRows
+                        .compactMap { $0.isVisiable ? $0.scrollViewWillBeginDragging?(scrollView, nil) : nil }
+        }
+    }
+
+    public func scrollViewDidEndDragging(_ scrollView: UIScrollView, willDecelerate decelerate: Bool) {
+        guard enableForwardScrollMsg else {
+            return
+        }
+        if let tv = scrollView as? UITableView, let proxy = tv.delegate as? GGTableViewDelegateProxy {
+            let _ = proxy.dataSource.allRows
+                        .compactMap { $0.isVisiable ? $0.scrollViewDidEndDragging?(scrollView, nil) : nil }
+        }
+    }
+
+    public func scrollViewWillBeginDecelerating(_ scrollView: UIScrollView) {
+        guard enableForwardScrollMsg else {
+            return
+        }
+        if let tv = scrollView as? UITableView, let proxy = tv.delegate as? GGTableViewDelegateProxy {
+            let _ = proxy.dataSource.allRows
+                        .compactMap { $0.isVisiable ? $0.scrollViewWillBeginDecelerating?(scrollView, nil) : nil }
+        }
+    }
+
+    public func scrollViewDidEndDecelerating(_ scrollView: UIScrollView) {
+        guard enableForwardScrollMsg else {
+            return
+        }
+        if let tv = scrollView as? UITableView, let proxy = tv.delegate as? GGTableViewDelegateProxy {
+            let _ = proxy.dataSource.allRows
+                        .compactMap { $0.isVisiable ? $0.scrollViewDidEndDecelerating?(scrollView, nil) : nil }
+        }
     }
 }
 
@@ -68,8 +187,93 @@ open class GGTableViewCell: UITableViewCell {
     }
 }
 
-//MARK:- section
-open class GGTableViewRow {
+
+
+//MARK:- base class GGNode
+public protocol GGNodeProtocol {
+    var indexPath: IndexPath { get set }
+    var tableView: UITableView? { get set }
+    
+}
+
+open class GGNode<Element>: GGNodeProtocol where Element: GGNodeProtocol {
+    
+    private var semaphore = DispatchSemaphore(value: 1)
+    
+    /*
+     * indexPath.section will be meaningless when GGNode self is GGTableViewSection.
+     * In other words, indexPath indicates that it has the same meaning with UITableViewCell's indexPath when current node is a GGTableViewRow,
+     * while GGTableViewSection only use indexPath.row (indexPath.section would always be zero)
+     */
+    public var indexPath = IndexPath(row: 0, section: 0)
+    
+    public weak var tableView: UITableView? {
+        didSet {
+            semaphore.wait()
+            for idx in 0..<items.count {
+                items[safe: idx].tableView = self.tableView
+            }
+            semaphore.signal()
+        }
+    }
+    
+    private var items = [Element]()
+    
+    public var count: Int {
+        items.count
+    }
+    
+    public init() {  }
+    
+    public subscript(_ index: Int) -> Element {
+        get {
+            items[safe: index]
+        }
+        set {
+            items[safe: index] = newValue
+        }
+    }
+}
+
+extension GGNode {
+    
+    var allItems: [Element] {
+        items
+    }
+    
+    func add(item: Element) {
+        semaphore.wait()
+        let index = items.count
+        items.append(item)
+        var temp = items[safe: index]
+        temp.indexPath = IndexPath(row: index, section: self.indexPath.row)
+        temp.tableView = tableView
+        semaphore.signal()
+    }
+    
+    func add(items: Array<Element>) {
+        for idx in 0..<items.count {
+            add(item: items[safe: idx])
+        }
+    }
+    
+    func remove(item: Element) {
+        semaphore.wait()
+        items.remove(safe: item.indexPath.row)
+        semaphore.signal()
+    }
+    
+    public func removeAll() {
+        semaphore.wait()
+        items.removeAll()
+        semaphore.signal()
+    }
+}
+
+//MARK:- row
+open class GGTableViewRow: GGNodeProtocol {
+    public weak var tableView: UITableView?
+    
     open var CellClass: UITableViewCell.Type {
         UITableViewCell.self
     }
@@ -78,13 +282,24 @@ open class GGTableViewRow {
         fatalError("Must Override")
     }
 
-    open var height:CGFloat = 0.0
+    public var height = CGFloat.zero
     open func cellHeight() -> CGFloat { height > 0 ? height : UITableView.automaticDimension }
     open func cellReuseIdentifier() -> String {
         String(describing: self)
     }
     
-    open var didSelectRow: ((IndexPath) -> Void)?
+    public var didSelectRow: GGTableViewProxyBlock?
+    public var didDeselectRow: GGTableViewProxyBlock?
+    public var willDisplayCell: GGTableViewProxyBlock?
+    public var didEndDisplayingCell: GGTableViewProxyBlock?
+    
+    public var scrollViewDidScroll: GGTableViewScrollBlock?
+    public var scrollViewWillBeginDragging: GGTableViewScrollBlock?
+    public var scrollViewDidEndDragging: GGTableViewScrollBlock?
+    public var scrollViewWillBeginDecelerating: GGTableViewScrollBlock?
+    public var scrollViewDidEndDecelerating: GGTableViewScrollBlock?
+    
+    public var indexPath: IndexPath = IndexPath(row: 0, section: 0)
     
     public init() {  }
     
@@ -96,18 +311,85 @@ open class GGTableViewRow {
             return cell
         }
     }
+    
+    public var isVisiable = false {
+        didSet {
+            print(indexPath, "====", isVisiable)
+        }
+    }
 }
 
-open class GGTableViewSection {
-    open var rowsArray: Array<GGTableViewRow>
-    public init() {
-        rowsArray = []
+//MARK:- section
+open class GGTableViewSection: GGNode<GGTableViewRow> {
+    
+    open var sectionHeaderView: UIView?
+    public var headerHeight = CGFloat.zero
+    open var sectionFooterView: UIView?
+    public var footerHeight = CGFloat.zero
+    
+    public var allRows: [GGTableViewRow] {
+        super.allItems
+    }
+    
+    public func add(row: GGTableViewRow) {
+        super.add(item: row)
+    }
+    
+    public func add(rows: Array<GGTableViewRow>) {
+        super.add(items: rows)
+    }
+    
+    public func remove(row: GGTableViewRow) {
+        super.remove(item: row)
+    }
+}
+
+open class GGTableViewDataSource: GGNode<GGTableViewSection> {
+    public var allRows: [GGTableViewRow] {
+        var ret = [GGTableViewRow]()
+        for sec in allSections {
+            ret.append(contentsOf: sec.allRows)
+        }
+        return ret
+    }
+    
+    public var allSections: [GGTableViewSection] {
+        allItems
+    }
+    
+    public func add(section: GGTableViewSection) {
+        super.add(item: section)
+    }
+    
+    public func add(sections: Array<GGTableViewSection>) {
+        super.add(items: sections)
+    }
+    
+    public func remove(section: GGTableViewSection) {
+        super.remove(item: section)
     }
 }
 
 //MARK:- Array subscript extension
 extension Array {
     subscript(safe index: Int) -> Element {
-        (0..<count).contains(index) ? self[index] : self[0]
+        get {
+            (0..<count).contains(index) ? self[index] : self[0]
+        }
+        set {
+            if (0..<count).contains(index) {
+                self[index] = newValue
+            } else {
+                assertionFailure("Array out of bounds")
+            }
+        }
+    }
+    
+    mutating func remove(safe index: Int) {
+        if (0..<count).contains(index) {
+            remove(at: index)
+        } else {
+            assertionFailure("Array out of bounds")
+        }
     }
 }
